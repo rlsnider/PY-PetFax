c63ffd6a03b7f42ac5cc4a09df674dadae2c1e19
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2023-01-15 15:35:01 -0800

    forgot to push project up

---
+++ petfax/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:34Shutthed**r@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+    
+    
+    
+    @app.route('/')
+    def index():
+        return "Hello, PetFax!"
+    
+    from . import pet
+    app.register_blueprint(pet.bp)
+    
+    from . import fact
+    app.register_blueprint(fact.bp)
+    
+    return app
---
+++ petfax/fact.py
@@ -0,0 +1,14 @@
+from flask import ( Blueprint, render_template, request, redirect ) 
+
+bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method =='POST':
+        print(request.form)
+        return redirect('/facts')
+    return 'This is the facts index'
+
+@bp.route('/new')
+def new(): 
+    return render_template('facts/new.html')
